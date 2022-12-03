424698c5e193dd2ef4224cc222e14814c1147a9c
Daniel <studiosdopplermusic@gmail.com>
2022-12-03 11:17:36 -0500

    etapas del desarrollo

---
+++ intro/01_etapas_de_desarrollo.js
@@ -0,0 +1,25 @@
+/**
+ * ETAPAS DEL DESARROLLO DE SOFTWARE
+ *
+ * ANALISIS DE REQUERIMIENTO
+ * - Se recolecta la ideas deacuerdo a la necesidad
+ * - Entender que se va a construir, entender los requerimientos de los usuarios funcionales y NO funcionales
+ * - ¿Que problema vamos a resolver?
+ *
+ * DISEÑO DE LA SOLUCIÓN
+ * - Modelo de la solucion
+ * - Abstracción del requerimiento
+ *
+ * DESARROLLO A EVALUACIÓN
+ * - Desarrollo y pruebas de la solución
+ * - Conocer los criterios de aceptación
+ *
+ * DESPLIEGUE
+ * - Arquitectura y roles de operación
+ * - Desplegar el aplicativo
+ *
+ * MANTEMANIENTO
+ * - Detención de errores y nuevas funcionales
+ *
+ *
+ * */
