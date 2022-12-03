f0620d4c3992d23c4c89dc945df4665562069763
Daniel <studiosdopplermusic@gmail.com>
2022-12-03 11:53:28 -0500

    que es la arquitectura

---
+++ 01_fundamentos/01_que_es_arquitectura.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * ¿QUE ES ARQUITECTURA DE SOFTWARE
+ *
+ * - La manera en la que que busca solucionar el problema mediante un fuerte analisis de la novedad
+ * - "La estructura del sistema, compuesta por elementos de software, sus propiedades visibles y sus relaciones"
+ * - "Es un conjunto de decisiones proncipales de diseños para el desarrollo del sistema"
+ * -  El contemplar todo lo importante para el diseño de la aplicación
+ *
+ *
+ * EJEMPLOS
+ * - Arquitectura de twitter
+ * - Flujo de datos flux
+ *
+ *
+ *
+ * */
