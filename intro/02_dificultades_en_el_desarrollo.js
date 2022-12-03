57c6227694e04e30a66044e4b1d3a0168e64637f
Daniel <studiosdopplermusic@gmail.com>
2022-12-03 11:33:34 -0500

    dificultades del desarrollo

---
+++ intro/02_dificultades_en_el_desarrollo.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * DIFICULTADES
+ *
+ *
+ * Primero debemos encontrar cuales son esos problemas que tenemos que resolver para posteriormente implementarlo
+ *
+ * PROBLEMAS ESENCIALES
+ * Entender el concepto y diseño de lo que vamos a solucionar y diseñar y se pueden dividir en:
+ * - Complejidad: que tan dificil es.
+ * - Conformidad: en que contexto se va usar y como se debe adecuar a ese contexto (conexión a internet, comunicación entre sowftware)
+ * - Tolerancia al cambio: el software va ser actualizable, escalable, o va ser estático, deacuerdo al cambio de la necesidad
+ * - Invisibilidad: cosas que estan implementadas fuera de la interface del software (codigo, api, servicios, etc)
+ *
+ * ¿Como solucionar las dificultades esenciales?
+ * - NO DESARROLLAR si ya hay una solución o usar OPEN SOURCE
+ * - PROTOTIPADO RAPIDO: obtener feedback lo antes posible de si estamos solucionando el problema (metodologias agiles)
+ * - DESARROLLO EVOLUTIVO: Crear pequeños desarrollos que poco a poco vayan evolucionando
+ * - GRANDES DISEÑADORES: Abstraer el problema general y solucionarlo de manera simple en los temas puntuales que este necesita
+ *
+ * PROBLEMAS ACCIDENTALES
+ * Tecnologías que vamos a implementar, donde lo vamos a alojar, frameworks, librerías, etc, estos problemas se dividen en:
+ * - Lenguages (alto ||
+ * - multiprocesamiento
+ * - entornos de programación
+ *
+ *
+ *
+ *
+ *
+ *
+ * */
