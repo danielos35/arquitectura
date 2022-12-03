901cc757ea1d5734b37277a3a06b81a16c8b9466
Daniel <studiosdopplermusic@gmail.com>
2022-12-03 11:42:54 -0500

    roles

---
+++ intro/03_roles.js
@@ -0,0 +1,22 @@
+/**
+ * ROLES INVOLUCRADOS EN EL PROCESO DE DESARROLLO DE SOFTWARE
+ *
+ *(Derecha, metodología tradiciona, izquierda metodologías agiles)
+ *
+ * EXPERTO DEL DOMINIO  | STAKEHOLDERS
+ * - Persona a la que acudimos para resolver las necesidades de los requerimientos
+ *
+ * ANALISTA | Cliente o Dueño del producto
+ * - Indaga y define los requerimientos del producto
+ *
+ * ADMINISTRADOR DE SISTEMAS | DEvops SRE (Ingeniero de la confianza del sitio)
+ * - Entender la infraestructura a la que va ir nuestra aplicación
+ *
+ * QA - TEST - DESARROLLADOR - ARQUITECTO | EQUIPO DE DESARROLLO
+ * - Crear el producto (en metodologías agiles NO existen estos roles)
+ *
+ * GESTOR DEL PROYECTO | SCRUM MASTER
+ * - Gestión del proyecto
+ *
+ *
+ * */
