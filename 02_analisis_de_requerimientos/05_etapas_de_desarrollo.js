2c398c92881b3f9afae67e2591e21d83b6bca828
Daniel <studiosdopplermusic@gmail.com>
2022-12-08 13:55:08 -0500

    etapas del desarrollo de software

---
+++ 02_analisis_de_requerimientos/05_etapas_de_desarrollo.js
@@ -0,0 +1,34 @@
+/**
+ *
+ * ETAPAS CLASICAS
+ *
+ * - Analisis de requerimientos
+ * -- Todo nace de un disparador, una idea o una necesidad
+ *  Output> compresion clara del problema que vamos a resolver
+ *
+ * - Diseño de la solución
+ * -- Analisis profundo de los problemas y una propuesta de solución de los mismos
+ *  Output> Solución detallada
+ *
+ * - Desarrollo y evolución
+ * -- Se hace realiza la tarea de programar la solución
+ *  OutPut> Artefactor de software (Programa) cumpliendo los criterios de aceptación
+ *
+ * - Despliegue
+ * --  Poner artefacto a disposición del cliente.
+ *  OutPut> Software disponible
+ *
+ * - Etapa de mantemiento
+ * -- Se atiende la detección de errores
+ *  OutPut> Soporta hasta el que el sotware queda deprecado
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * */
