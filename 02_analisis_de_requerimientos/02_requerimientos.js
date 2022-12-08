496f6fe411e3ae5d0d2d90b86fb0300d0c1fe554
Daniel <studiosdopplermusic@gmail.com>
2022-12-07 23:08:21 -0500

    riesgos

---
+++ 02_analisis_de_requerimientos/02_requerimientos.js
@@ -0,0 +1,46 @@
+/**
+ *
+ * Requerimientos
+ *
+ * NEGOCIO
+ * - Capa de requerimiento de negocios
+ * --- Requerimiento de negocio
+ * --- reglas de negocio
+ *
+ * USUARIO
+ * (Como el usuario se puede desenvolver usando el sistema)
+ * - Capa de requerimiento de usuario
+ * --- Atributos de calidad: Cuales son los mas importantes
+ * --- Requerimientos No funcionales
+ *
+ * FUNCIONAL
+ * - Requerimientos del sistema
+ * - Requerimientos funcionales
+ * - Restricciones
+ *
+ *
+ * REQUERIMIENTOS DE PROYECTO (Tienen que ver con el rol de gestor de proyecto)
+ * - Recursos
+ * - Capacitación
+ * - Certificaciones
+ * - Documentación de usuario
+ * - Infraestructura
+ * - Licencias
+ * - Plan de despliegue
+ * - Plan de transición
+ * - Acuerdos de servicios
+ *
+ * REQUERIMIENTOS FUNCIONES
+ * - Como el sistema se va comportar bajo un estimulo || Como implementar una historia
+ *
+ * REQUERIMIENTOS NO FUNCIONALES
+ * - Atributos de calidad
+ * - Seguridad
+ *
+ *
+ * REQUERIMIENTOS SIGNIFICATIVOS
+ * - Agrupan a los requerimientos FUNCIONALES y NO FUNCIONES
+ *
+ *
+ *
+ * */
